8e97e3d8d0b9c47ca0c1003503bec2f1902d0653
syedahmedkhaderi <syedahmedkhaderi@gmail.com>
2025-10-13 17:17:35 +0300

    SLL & DLL Modified

--- 6-LinkedList/1-Singly LL/singly_linked_list.py
+++ 6-LinkedList/1-Singly LL/singly_linked_list.py
@@ -1,3 +1,17 @@
+#Linked lists are, as the name suggests, a list which is linked.
+#It consists of nodes which contain data and a pointer to the next node in the list.
+#The list is connected with the help of these pointers.
+#These nodes are scattered in memory, quite like the buckets in a hash table.
+#The node where the list starts is called the head of theblist and the node where it ends, or last node, is called the tail of the list.
+#The average time complexity of some operations invloving linked lists are as follows:
+#Look-up : O(n)
+#Insert : O(n)
+#Delete : O(n)
+#Append : O(1)
+#Prepend : O(1)
+#Python doesn't have a built-in implementation of linked lists, we have to build it on our own
+#So, here we go.
+
 class Node:
     def __init__(self, value):
         self.value = value
@@ -168,7 +182,7 @@ print(ll) # __Str__ Method
 
 
 '''
-Linear search for linked lists works the same as for arrays. 
+Linear search for linked lists works the same as for arrays.
 A list of unsorted values are traversed from the head node until the node with the specific value is found. Time complexity is O(n).
 
 Binary search is not possible for linked lists because the algorithm is based on jumping directly to different array elements, 
--- 6-LinkedList/2-Doubly LL/doubly_linked_list.py
+++ 6-LinkedList/2-Doubly LL/doubly_linked_list.py
@@ -1,29 +1,44 @@
+#Doubly linked lists are just normal, singly linked lists with one added feature,
+#a link to the previous node as well in addition to a link to the next node.
+#Although the worst case time complexities of all operations in a doubly linked list are same as that of a singly linked list,
+#Some operations are technically faster. For example, lookup or searching, is O(n/2) as search can begin from both ends
+#But O(n/2) = O(n), so it is still the same as that for a singly linked list.
+
+#Implementation of doubly linked list is almost exactly the same as that for singly linked list,
+#With just the added feature of the pointer to the previous node.
+#We'll have the same methods which do the exact same thing. The pars which will be different from the singly linked list are explained
+#So lets implement it.
+
 class Node:
     def __init__(self, value):
+        # Represents a node in the DLL with value and next/prev pointers
         self.value = value
         self.next = None
         self.prev = None
 
 class DoublyLinkedList:
     def __init__(self):
+        # Initialize an empty DLL with head/tail and size tracking
         self.head = None
         self.tail = None
         self.prev = None
         self.length = 0
 
     def append(self, value):
+        # Add a node to the end; update tail and link prev/next pointers
         new_node = Node(value)
         if self.head is None:
             self.head = new_node
             self.tail = self.head
 
         else:
-            self.prev = self.tail
+            new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
         self.length += 1
 
     def prepend(self, value):
+        # Add a node to the beginning; update head and link pointers
         new_node = Node(value)
         if self.length == 0:
             self.head = new_node
@@ -35,6 +50,7 @@ class DoublyLinkedList:
         self.length += 1
 
     def insert(self, index, value):
+        # Insert at index; prepends/appends when out of range boundaries
         new_node = Node(value)
 
         if index <= 0:
@@ -75,6 +91,7 @@ class DoublyLinkedList:
         '''
 
     def traversetoindex(self, index):
+        # Return node at index by walking from head
         curr_node = self.head
         i = 0
         while i != index:
@@ -83,6 +100,7 @@ class DoublyLinkedList:
         return curr_node
 
     def delete(self, index):
+        # Delete node at index; updates head/tail and relinks neighbors
         temp = self.head
         i = 0
         if index < 0 or index >= self.length:
@@ -94,6 +112,8 @@ class DoublyLinkedList:
             self.length -= 1
             if self.length == 0:
                 self.tail = None  # Handle empty list
+            else:
+                self.head.prev = None
             return
 
         if index == self.length - 1:
@@ -129,13 +149,65 @@ class DoublyLinkedList:
         self.length -= 1
         '''
 
+    def search(self, target):
+        # Linear search for value; returns index or -1 if not found
+        current_node = self.head
+        index = 0
+        while current_node:
+            if current_node.value == target:
+                return index
+            current_node = current_node.next
+            index += 1
+        return -1
+
+    def set_value(self, index, value):
+        # Set a node's value at index; returns True if successful
+        node = self.traversetoindex(index) if 0 <= index < self.length else None
+        if node:
+            node.value = value
+            return True
+        return False
+
+    def clear_DLL(self):
+        # Remove all nodes and reset the list
+        self.head = None
+        self.tail = None
+        self.length = 0
+
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return None
+
+        if index < self.length // 2:
+            current_node = self.head
+            for _ in range(index):
+                current_node = current_node.next
+        else:
+            current_node = self.tail
+            for _ in range(self.length - 1, index, -1):
+                current_node = current_node.prev
+        return current_node
+
     def printl(self):
+        # Return a list-like string of values with the current length
         arr = []
         temp = self.head
         while temp is not None:
             arr.append(temp.value)
             temp = temp.next
         return f"{arr} | Length = {str(self.length)}"
+
+    
+    def __str__(self) -> str:
+        temp_node = self.head
+        result = ""
+        while temp_node:
+            result += str(temp_node.value)
+            if temp_node.next is not None:
+                result += "<->"
+            temp_node = temp_node.next
+        return result
+
 
 
 d = DoublyLinkedList()
