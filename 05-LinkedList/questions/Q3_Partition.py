7129d3cef71b5bf4bbe2b18782f302b8a86edf12
syedahmedkhaderi <syedahmedkhaderi@gmail.com>
2026-05-02 21:53:36 +0300

    Completed LC 160 & 86

--- 05-LinkedList/questions/Q3_Partition.py
+++ 05-LinkedList/questions/Q3_Partition.py
@@ -29,3 +29,42 @@ customLL.generate(10,0,99)
 print(customLL)
 partition(customLL, 30)
 print(customLL)
+
+
+''' My unqiue solulu
+Time Complexity: O(n)
+Space Complexity: O(1)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def partition(self, head: Optional[ListNode], x: int) -> Optional[ListNode]:
+        before = ListNode(0)   # < x
+        after = ListNode(0)    # >= x
+
+        before_curr = before
+        after_curr = after
+
+        curr = head
+
+        while curr:
+            next_node = curr.next   # Save next
+            curr.next = None        # Detach node
+
+            if curr.val < x:
+                before_curr.next = curr
+                before_curr = before_curr.next
+            else:
+                after_curr.next = curr
+                after_curr = after_curr.next
+
+            curr = next_node
+
+        before_curr.next = after.next
+
+        return before.next
+            
+'''
--- 05-LinkedList/questions/Q5_Intersection.py
+++ 05-LinkedList/questions/Q5_Intersection.py
@@ -53,5 +53,72 @@ print(intersection(llA, llB))
 
 
 
+''' Hashtable approach
 
-    
+Time Complexity: O(n + m) ; n = len(A), m = len(B)
+Space Complexity: O(n)
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        seen = set()
+
+        curr1 = headA
+        while curr1:
+            seen.add(curr1)
+            curr1 = curr1.next
+        
+        curr2 = headB
+        while curr2:
+            if curr2 in seen:
+                return curr2
+            curr2 = curr2.next
+
+        return None
+'''
+
+''' Longer list approach
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def getIntersectionNode(self, headA: ListNode, headB: ListNode) -> Optional[ListNode]:
+        def get_length(head):
+            length = 0
+            while head:
+                length += 1
+                head = head.next
+            return length
+        
+        # Get the lengths of both ll
+        len_a = get_length(headA)
+        len_b = get_length(headB)
+
+        a, b = headA, headB
+
+        # Move longer list forward
+        if len_a > len_b:
+            for _ in range(len_a - len_b):
+                a = a.next
+        else:
+            for _ in range(len_b - len_a):
+                b = b.next
+
+        # now traverse both lists comparing the elements.
+        while a or b:
+            if a == b:
+                return a
+            a = a.next
+            b = b.next
+
+        return None
+
+'''
